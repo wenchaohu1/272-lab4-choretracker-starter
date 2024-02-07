572a4b0cb08621ec449a4696aeed200c0e8836aa
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-02-06 19:41:12 -0500

    Passed all tests for task_test file

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ApplicationRecord
 
     has_many :chores
     has_many :children, through: :chores
+
+    # Validations
+    validates_presence_of :name
+
+    # Scope
+    scope :active, -> { where(active: true) } 
+    scope :alphabetical, -> { order(name: :asc) }
+
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
-
+require "test_helper" 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Test realtionshpis
+  should have_many(:chores)
+  should have_many(:children).through(:chores)
+
+  # Validations
+  should validate_presence_of(:name) 
+
+  test "should order tasks alphabetically by name" do
+    task1 = Task.create(name: "Task B", points: 5, active: true)
+    task2 = Task.create(name: "Task A", points: 10, active: true)
+    task3 = Task.create(name: "Task C", points: 8, active: true)
+    
+    assert_equal [task2, task1, task3], Task.alphabetical
+  end
+
+  test "should select only active tasks" do
+    active_task = Task.create(name: "Active Task", points: 5, active: true)
+    inactive_task = Task.create(name: "Inactive Task", points: 10, active: false)
+
+    assert_includes Task.active, active_task
+    refute_includes Task.active, inactive_task
+  end
 end
