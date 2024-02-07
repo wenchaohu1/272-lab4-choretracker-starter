f8c8be2d59c0c2cb9fb8f984374501f2d1f8ee77
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-02-06 19:59:58 -0500

    passed all chore_test

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,23 @@
 class Chore < ApplicationRecord 
+    # Relationships
     belongs_to :child
     belongs_to :task
+
+    # Validations
+    validates_datetime :due_on 
+
+    # Scopes 
+    scope :by_task, -> { joins(:task).order('tasks.name') }
+    scope :chronological, -> { order('due_on') } 
+
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where('due_on >= ?', Date.today) }
+    scope :past, -> { where('due_on < ?', Date.today) }
+
+    # Method 
+    def status
+        completed ? "Completed" : "Pending"
+    end
+
 end
